0f626e15df2434758f8c7c6674b2cffe785ed97d
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-12-13 11:28:30 -0500

    Update and rename cm.mk to ose.mk

--- ose.mk
+++ ose.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OSE stuff.
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vs980/vs980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs980
-PRODUCT_NAME := cm_vs980
+PRODUCT_NAME := ose_vs980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS980
 PRODUCT_MANUFACTURER := lge
@@ -23,5 +23,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="g2_vzw-user 4.4.2 KOT49I.VS98024A VS98024A.1394607468 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
-
-PRODUCT_PACKAGES += Torch
