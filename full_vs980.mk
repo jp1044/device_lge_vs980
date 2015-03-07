9faf514e77453c80bd905050b6cb1311c367037c
nobe1976 <charlesmc76@gmail.com>
2015-03-06 19:15:30 -0500

    Modifies for AOD
    Also reworked system prop

--- full_vs980.mk
+++ full_vs980.mk
@@ -7,16 +7,10 @@ TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs980
-PRODUCT_NAME := aosp_vs980
+PRODUCT_NAME := full_vs980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS980
 PRODUCT_MANUFACTURER := lge
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=lge/g2_vzw/g2:4.4.2/KOT49I.VS98024A/VS98024A.1394607468:user/release-keys \
-    PRIVATE_BUILD_DESC="g2_vzw-user 4.4.2 KOT49I.VS98024A VS98024A.1394607468 release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vs980/device.mk)
